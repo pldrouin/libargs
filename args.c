824ede4e9594890c49817b0cbc1322c0f2d735d8
Pierre-Luc Drouin <pldrouin@pldrouin.net>
2024-03-03 23:12:36 -0500

    getnextparam did not work properly when reading a configuration file with additional executable arguments, due to the way the prompt mode was implemented. It should now be fixed.

--- args.c
+++ args.c
@@ -20,24 +20,23 @@ int args_open_conf_file(args_info* a_h, const char* filename)
   if(!argsdiffer(filename,"-")) {
 
     if(a_h->fptri==-1 || a_h->fptra[a_h->fptri] != stdin) {
-      a_h->fptri++;
-      a_h->fptra=(FILE**)realloc(a_h->fptra,(a_h->fptri+1)*sizeof(FILE*));
+      a_h->fptra=(FILE**)realloc(a_h->fptra,(a_h->fptri+2)*sizeof(FILE*));
 
       if(a_h->fptra == NULL) return errno;
+      ++(a_h->fptri);
       a_h->fptra[a_h->fptri]=stdin;
     }
 
   } else {
-    ++(a_h->fptri);
-    a_h->fptra=(FILE**)realloc(a_h->fptra,(a_h->fptri+1)*sizeof(FILE*));
+    a_h->fptra=(FILE**)realloc(a_h->fptra,(a_h->fptri+2)*sizeof(FILE*));
 
     if(a_h->fptra == NULL) return errno;
 
-    if(!(a_h->fptra[a_h->fptri]=fopen(filename,"r"))) {
+    if(!(a_h->fptra[a_h->fptri+1]=fopen(filename,"r"))) {
       fprintf(stderr,"%s: Error: Cannot open file '%s' in read mode\n",__func__,filename);
-      --(a_h->fptri);
       return errno;
     }
+    ++(a_h->fptri);
   }
   return 0;
 }
@@ -48,14 +47,17 @@ int args_prompt(args_info* a_h, const char* prompt, char* pbuf, const int maxlen
   char* charptr;
   size_t n;
   ssize_t ret;
-  int oldnargs=a_h->nargs;
-  const int initptri=a_h->fptri;
   const char** oldargs=a_h->args;
-  int oldparc=a_h->parc;
   int plength;
-  a_h->nargs=-1;
-  a_h->parc=-2;
   a_h->args=(char const**)&charptr;
+
+  a_h->fptra=(FILE**)realloc(a_h->fptra,(a_h->fptri+2)*sizeof(FILE*));
+
+  if(a_h->fptra == NULL) return errno;
+  ++(a_h->fptri);
+  a_h->fptra[a_h->fptri]=NULL;
+
+  const int initptri=a_h->fptri;
 
   int cret;
 
@@ -69,9 +71,8 @@ next_line:
 
     if(ret<0) {
       free(lineptr);
-      a_h->nargs=oldnargs;
       a_h->args=oldargs;
-      a_h->parc=oldparc;
+      --(a_h->fptri);
       break;
     }
 
@@ -79,9 +80,6 @@ next_line:
       //printf("Inner loop\n");
       //Prioritise reading from prompt if a new configuration file has not been loaded from prompt
 
-      if(a_h->fptri==initptri) a_h->parc=-2;
-
-      else a_h->parc=-1;
       plength=getnextparam(a_h,false,pbuf,maxlen);
 
       if(plength<=0) {
@@ -118,9 +116,8 @@ next_line:
 exit_prompt:
   free(lineptr);
   printf("\n");
-  a_h->nargs=oldnargs;
   a_h->args=oldargs;
-  a_h->parc=oldparc;
+  --(a_h->fptri);
   return 0;
 }
 
@@ -128,16 +125,16 @@ int getnextparam(args_info* a_h, const bool isarg, char *param, const int maxlen
 {
   int i=0;
   //Priority order is:
-  //1) Reading from a string
   //1) Reading from file handles
-  //2) Reading from arguments
-  //Reading from a string occur when nargs<0, in which case args[0] is assumed
+  //2) Reading from a string
+  //3) Reading from arguments
+  //Reading from a string occur when fptri>0 and fptra[fptri]=NULL, in which case args[0] is assumed
   //to point to the current character to be read.
 
   while(a_h->fptri>-1 || a_h->nargs>a_h->parc) {
 
     //If reading from args
-    if(a_h->nargs>=0 && a_h->fptri==-1) {
+    if(a_h->fptri==-1) {
       if(!isarg) while(a_h->args[a_h->parc][i] == '-') i++;
       int j=strlen(a_h->args[a_h->parc]+i);
       j=(j>=maxlen?maxlen-1:j);
@@ -147,13 +144,12 @@ int getnextparam(args_info* a_h, const bool isarg, char *param, const int maxlen
       //printf("param is '%s'\n",param);
       return j;
 
-    //Else if (a_h->nargs<0 && a_h->nargs>a_h->parc) || a_h->fptri!=-1
     } else {
       signed char c;
       bool brs=false, brd=false, com=false;
 
       //If reading from file
-      if(a_h->nargs<=a_h->parc) {
+      if(a_h->fptra[a_h->fptri]!=NULL) {
         //printf("Reading from file level %i\n",a_h->fptri);
 
 	while(isspace((c=fgetc(a_h->fptra[a_h->fptri]))) || ((c == '=' || c == ':') && isarg) || c == '#' || (com && c != 0 && c != EOF)) {if(com && c == '\n') com=false; if(c == '#') com=true;}
@@ -247,6 +243,6 @@ void args_close_all_files_down_to_level(args_info* a_h, const int ptri_stop)
 {
   for(; a_h->fptri>ptri_stop; --(a_h->fptri)) {
 
-    if(a_h->fptra[a_h->fptri]!=stdin) fclose(a_h->fptra[a_h->fptri]);
+    if(a_h->fptra[a_h->fptri]!=NULL && a_h->fptra[a_h->fptri]!=stdin) fclose(a_h->fptra[a_h->fptri]);
   }
 }
